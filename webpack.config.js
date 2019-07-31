78d8a9ba567c7e6ae04f4724aea7bff9c0826904
hagaitski <i.haga629@gmail.com>
2019-07-31 23:33:19 +0900

    ts-loaderに変更

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
                 test: [/\.js$/,/\.ts$/],
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+                test: [/\.ts$/],
+                exclude: /node_modules/,
+                loader: 'ts-loader'
             }
         ]
     },
