d00c51ef203a225d09018deb4d706958bd60913e
hagaitski <i.haga629@gmail.com>
2019-10-12 14:50:53 +0900

    少し修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const file = (...args) => path.resolve(__dirname,...args)
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const file = (...args) => path.resolve(__dirname, ...args);
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: ['@babel/polyfill',file("src","index.ts")]
-    },
-    output: {
-        path: file("dist"),
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: [/\.js$/,/\.ts$/],
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: [/\.ts$/],
-                exclude: /node_modules/,
-                loader: 'ts-loader'
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: ['.js', '.jsx','.ts','.tsx'],
-        modules: ['node_modules'],
-        alias: {
-            '@': file("src")
-        }
-    },
-    devServer: {
-        port: 3000,
-        historyApiFallback:true,
-        open:false
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: file("src",'index.html'),
-            filename: 'index.html'
-        })
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill", file("src", "index.ts")]
+  },
+  output: {
+    path: file("dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.ts$/],
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: [/\.ts$/],
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }
     ]
-}
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: ["node_modules"],
+    alias: {
+      "@": file("src")
+    }
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    open: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: file("src", "index.html"),
+      filename: "index.html"
+    })
+  ]
+};
